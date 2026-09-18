90376c42b83199483ac7af2336dffdbd8a8be92d
unedtamps <unedo.tampubolon@gmail.com>
2026-09-18 17:02:03 +0700

    argocd default config

---
+++ aws_kube/k8s/platform/argocd/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+helm upgrade --install argocd argo/argo-cd \
+  --namespace argocd \
+  --create-namespace
